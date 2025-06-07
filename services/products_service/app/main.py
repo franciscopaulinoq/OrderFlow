e8355f21d0a3fd3648e134625680d39c7de759bb
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-06-07 20:38:40 -0300

    feat: (products_service) Implement POST /products and GET /products

--- services/products_service/app/main.py
+++ services/products_service/app/main.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, status
+from sqlalchemy.orm import Session
 
-from .database import Base, engine
+from . import crud, schemas
+from .database import Base, engine, get_db
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,8 +18,18 @@ app = FastAPI(
     title="Products Service",
     description="API for managing product catalog.",
     version="0.0.1",
+    lifespan=lifespan
 )
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to Product Service!"}
+
+@app.post("/products/", response_model=schemas.Product, status_code=status.HTTP_201_CREATED)
+def create_product_endpoint(product: schemas.ProductBase, db: Session = Depends(get_db)):
+    return crud.create_product(db=db, product=product)
+
+@app.get("/products/", response_model=list[schemas.Product])
+def read_products_endpoint(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    products = crud.get_products(db, skip=skip, limit=limit)
+    return products
